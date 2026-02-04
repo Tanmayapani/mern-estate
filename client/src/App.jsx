17f46aef6b8511217a908c8b6e904b97fd9ae2dd
Tanmaya Kumar Pani <tanmayapani01@gmail.com>
2026-02-04 14:54:20 +0530

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Profile from "./pages/Profile.jsx"
 import SignIn from "./pages/SignIn.jsx"
-import SignOut from "./pages/SignOut.jsx"
+import SignUp from "./pages/SignUp.jsx"
 import Header from "./components/Header.jsx"
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
       </BrowserRouter>
   )
