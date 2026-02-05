5263fb74e958ce0920ef19e817878f26108b9230
Tanmaya Kumar Pani <tanmayapani01@gmail.com>
2026-02-05 18:59:39 +0530

    update the header and make the profile page private

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,6 +38,7 @@ export const google = async (req, res, next) => {
         const user = await User.findOne({ email: req.body.email });
         if (user) {
             const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const { password: pwd, ...rest } = user._doc;
             res.cookie('access_token', token, { httpOnly: true })
                .status(200)
                .json(rest);
