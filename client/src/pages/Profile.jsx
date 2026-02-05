505a70e7767de31b0937cbe9f9f246c57ac0469c
Tanmaya Kumar Pani <tanmayapani01@gmail.com>
2026-02-06 02:27:14 +0530

    create sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,8 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useRef, useState, useEffect } from "react";
-import { updateUserStart, updateUserSuccess, 
-  updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from "../redux/user/userSlice";
+import { updateUserStart, updateUserSuccess, updateUserFailure, 
+  deleteUserStart, deleteUserSuccess, deleteUserFailure, 
+  signOutUserStart, signOutUserSuccess, signOutUserFailure } from "../redux/user/userSlice";
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -101,6 +102,24 @@ export default function Profile() {
     }
   }
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout', {
+        method: 'GET',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -170,7 +189,7 @@ export default function Profile() {
 
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-600 cursor-pointer">Delete Account</span>
-        <span className="text-red-600 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-600 cursor-pointer">Sign Out</span>
       </div>
       
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
